89af5f621f38202cab1f60bdc94cff30002c3481
Nudra Pradivta <nudrap0503@gmail.com>
2025-04-06 21:32:59 +0700

    Add api documentation for login and register

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import express from "express";
 import router from "./routes/api";
 import bodyParser from "body-parser";
 import db from "./utils/database";
+import docs from "./docs/route";
+import cors from "cors";
 
 async function init() {
   try {
     const result = await db();
     console.log("database status:", result);
     const app = express();
-
+    app.use(cors());
     app.use(bodyParser.json());
 
     const PORT = 3000;
@@ -21,6 +23,7 @@ async function init() {
     });
 
     app.use("/api", router);
+    docs(app);
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
